8f9b2e90cf23e277b35f53053d4d5ee81f322b6b
KholoudELkasaby <lilelksaby@gmail.com>
2025-05-05 23:06:19 +0300

    feat: add navbar with routing integration and lucide-angular icons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'signup', pathMatch: 'full' },
+  { path: '', component: ProjectsComponent },
   { path: 'signup', component: SignUpComponent },
   { path: 'login', component: SignInComponent },
   { path: '**', component: SignUpComponent },
