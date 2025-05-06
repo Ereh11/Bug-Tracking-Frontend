ea5801591465769d43d1255f834dcfd72dbf3747
KholoudELkasaby <lilelksaby@gmail.com>
2025-05-06 08:37:09 +0300

    feat: implement add project functionality with input validation and popup cancellation

--- src/app/components/projects/add-btn/add-btn.component.ts
+++ src/app/components/projects/add-btn/add-btn.component.ts
@@ -1,10 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-add-btn',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, CommonModule, FormsModule],
   templateUrl: './add-btn.component.html',
   styleUrl: './add-btn.component.css',
 })
-export class AddBtnComponent {}
+export class AddBtnComponent {
+  showAddProjectPopup = false;
+  showNameError = false;
+
+  newProject = {
+    name: '',
+    description: '',
+  };
+
+  createProject() {
+    if (!this.newProject.name.trim()) {
+      this.showNameError = true;
+      return;
+    }
+    this.showAddProjectPopup = false;
+    this.newProject = { name: '', description: '' };
+    this.showNameError = false;
+  }
+
+  cancelAddProject() {
+    this.showAddProjectPopup = false;
+    this.newProject = { name: '', description: '' };
+  }
+}
