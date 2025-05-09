062b3fd05c6804748da76b0f1a07a981be4740a3
KholoudELkasaby <lilelksaby@gmail.com>
2025-05-10 00:43:29 +0300

    feat: update project details component with structured layout and bug data integration; change page title to "Bug Ticket"

--- src/app/components/project-details/project-details.component.ts
+++ src/app/components/project-details/project-details.component.ts
@@ -1,11 +1,51 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-project-details',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project-details.component.html',
-  styleUrl: './project-details.component.css'
+  styleUrl: './project-details.component.css',
 })
 export class ProjectDetailsComponent {
-
+  managerName = 'Manager Name';
+  managerEmail = 'manager@example.com';
+  bugs = [
+    {
+      title: 'Login button not working',
+      status: 'New',
+      priority: 'Low',
+      assignedTo: null,
+    },
+    {
+      title: 'Page not rendering correctly on mobile',
+      status: 'In Progress',
+      priority: 'Medium',
+      assignedTo: null,
+    },
+    {
+      title: 'Payment processing error',
+      status: 'Resolved',
+      priority: 'High',
+      assignedTo: 'Kholoud',
+    },
+    {
+      title: 'Payment processing error',
+      status: 'Assigned',
+      priority: 'Critical',
+      assignedTo: 'Hani',
+    },
+    {
+      title: 'Payment processing error',
+      status: 'Closed',
+      priority: 'High',
+      assignedTo: 'Hani',
+    },
+    {
+      title: 'Payment processing error',
+      status: 'Reopened',
+      priority: 'Low',
+      assignedTo: null,
+    },
+  ];
 }
