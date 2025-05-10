a99af9d1a3fc918a87bbc7a8f55d453bf442b2be
KholoudELkasaby <lilelksaby@gmail.com>
2025-05-10 20:29:52 +0300

    feat: implement comment functionality in ticket component with popup for user interaction

--- src/app/components/project-btns/add-ticket/add-ticket.component.ts
+++ src/app/components/project-btns/add-ticket/add-ticket.component.ts
@@ -1,11 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-ticket',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-ticket.component.html',
-  styleUrl: './add-ticket.component.css'
+  styleUrl: './add-ticket.component.css',
 })
 export class AddTicketComponent {
+  showCommentPopup = false;
+  newComment = '';
+  comments: any[] = [];
 
+  user = {
+    name: 'John Smith',
+    color: 'bg-primary',
+  };
+
+  toggleCommentPopup() {
+    this.showCommentPopup = !this.showCommentPopup;
+  }
+
+  addComment() {
+    if (!this.newComment.trim()) return;
+
+    this.comments.push({
+      name: this.user.name,
+      color: this.user.color,
+      text: this.newComment.trim(),
+      date: new Date(),
+    });
+
+    this.newComment = '';
+  }
 }
