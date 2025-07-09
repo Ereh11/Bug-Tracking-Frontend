bd178602f838e5e51e336a97a31b9aa4739fdab3
KholoudELkasaby <lilelksaby@gmail.com>
2025-07-09 20:19:44 +0300

    refactor: remove server-side rendering support and related configurations
    - Deleted server-side rendering configuration files and dependencies.
    - Removed unused imports and providers from main application files.
    - Updated package.json to remove SSR-related packages and scripts.
    - Cleaned up TypeScript configuration to reflect changes in application structure.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { importProvidersFrom } from '@angular/core';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(withFetch()),
-    provideRouter(routes),
-    provideAnimations(),
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
